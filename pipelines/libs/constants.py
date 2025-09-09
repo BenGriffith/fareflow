5f49e87306ef33d2f979c9d611a8318da1751353
Ben Griffith <bengriffith@outlook.com>
2025-09-09 16:16:58 -0600

    add constants

--- pipelines/libs/constants.py
+++ pipelines/libs/constants.py
@@ -6,3 +6,7 @@ MINIO_ENDPOINT = config("MINIO_ENDPOINT")
 MINIO_ACCESS_KEY = config("MINIO_ACCESS_KEY")
 MINIO_SECRET_KEY = config("MINIO_SECRET_KEY")
 BASE_URL = "https://d37ci6vzurychx.cloudfront.net/trip-data"
+RAW = f"{DATA_ROOT}/raw"
+STAGING = f"{DATA_ROOT}/staging"
+AUDIT = f"{DATA_ROOT}/audit"
+CURATED = f"{DATA_ROOT}/curated"
